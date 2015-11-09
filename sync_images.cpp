ff41715529f90086fe75707a9adb90421c839e31
madewithlinux <madewithlinux.josh@gmail.com>
2015-11-09 16:28:19 -0600

    Removed all magic numbers now uses a JSON config file the only command line argument is the path to this config file

--- sync_images.cpp
+++ sync_images.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <list>
+#include <boost/property_tree/json_parser.hpp>
 #include <cstring> // memcmp, memset ...
 #include <ctime> // time
 #include <thread>  // threads
@@ -28,22 +29,35 @@ g++ -O3 -Wall -Wextra -lpthread -std=gnu++14 lib/coreutils/lib/sha512.c sync_ima
 typedef unsigned char uchar;
 typedef unsigned long long int ullong;
 typedef long long int llong;
+typedef boost::exception_detail::clone_impl<boost::exception_detail::error_info_injector<boost::property_tree::ptree_bad_data>> cfg_field_not_found;
 
+struct config_struct {
+    std::atomic_ullong current_block;
+    std::string input_file_path;
+    std::string output_file_path;
+    std::string hash_file_path;
+    ullong n_blocks;
+    ullong blocksize;
+    char *empty_block;
+    char *empty_hash;
+    int output_interval;
+    int thread_cout;
+    bool use_sparse_output;
+    bool do_status_update;
+};
 
-const int THREAD_COUNT = 4;
-const llong OUTPUT_INTERVAL = 1;
-
-void progress_thread(std::atomic_ullong &current_block_atomic, const ullong n_blocks) {
+void progress_thread(config_struct &cfg) {
     time_t start_time = std::time(0);
     long eta;
     long eta_min = 0;
     long eta_sec = 0;
     long eta_h = 0;
     double progress_precentage = 0;
+    // the loop condition is updated by hash_thread, so we ask clang to stop whining
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wfor-loop-analysis"
-    while (current_block_atomic < n_blocks) {
-        progress_precentage = 1.0 * current_block_atomic / n_blocks;
+    while (cfg.current_block < cfg.n_blocks) {
+        progress_precentage = 1.0 * cfg.current_block / cfg.n_blocks;
         eta = (long) ((time(0) - start_time) * (1.0 - progress_precentage) / progress_precentage);
         eta_sec = eta % 60;
         eta_min = (eta / 60) % 60;
@@ -56,55 +70,45 @@ void progress_thread(std::atomic_ullong &current_block_atomic, const ullong n_bl
         std::cout << eta_min << "m ";
         std::cout << eta_sec << "s";
         std::cout.flush();
-        sleep(OUTPUT_INTERVAL);
+        sleep((unsigned int) cfg.output_interval);
     }
 #pragma clang diagnostic pop
     // pad output with spaces to make sure that we clear the eta timer
     std::cout << "\rDone                            " << std::endl;
 };
 
-void hash_thread(
-        std::atomic_ullong &current_block_atomic,
-        const std::string &input_filename,
-        const std::string &output_filename,
-        const ullong n_blocks,
-        const ullong blocksize,
-        const char *empty_block,
-        const uint64_t *empty_hash_uint64_t
-) {
-
-    std::string hash_filename = output_filename + ".hash";
-    std::FILE *input_file = fopen(input_filename.c_str(), "r+");
-    std::FILE *hash_file = fopen(hash_filename.c_str(), "r+");
-    std::FILE *output_file = fopen(output_filename.c_str(), "r+");
+void hash_thread(config_struct &cfg) {
+    std::FILE *input_file = fopen(cfg.input_file_path.c_str(), "r+");
+    std::FILE *output_file = fopen(cfg.output_file_path.c_str(), "r+");
+    std::FILE *hash_file = fopen(cfg.hash_file_path.c_str(), "r+");
     std::size_t rw_size;
 
     char new_hash[SHA512_DIGEST_SIZE];
     char existing_hash[SHA512_DIGEST_SIZE];
-    char block_buffer[blocksize];
+    char block_buffer[cfg.blocksize];
 
-    // do this funky comparison because we must only interact with current_block_atomic once per loop
+    // do this funky comparison because we must only interact with current_block once per loop
     ullong current_block;
-    while ((current_block = current_block_atomic.fetch_add(1)) < n_blocks) {
+    while ((current_block = cfg.current_block.fetch_add(1)) < cfg.n_blocks) {
         memset(existing_hash, 0, SHA512_DIGEST_SIZE);
-        memset(block_buffer, 0, blocksize);
-        std::fseek(input_file, current_block * blocksize, SEEK_SET);
-        rw_size = std::fread(block_buffer, 1, blocksize, input_file);
+        memset(block_buffer, 0, cfg.blocksize);
+        std::fseek(input_file, current_block * cfg.blocksize, SEEK_SET);
+        rw_size = std::fread(block_buffer, 1, cfg.blocksize, input_file);
         std::fseek(hash_file, current_block * SHA512_DIGEST_SIZE, SEEK_SET);
         std::fread(existing_hash, 1, SHA512_DIGEST_SIZE, hash_file);
 
         // check if block is empty, and if so, don't even hash it
-        if (memcmp(block_buffer, empty_block, blocksize) == 0) { // block is empty
+        if (cfg.use_sparse_output && memcmp(block_buffer, cfg.empty_block, cfg.blocksize) == 0) { // block is empty
             // only write sparse blocks if their hash does not match
-            if (memcmp(existing_hash, empty_hash_uint64_t, SHA512_DIGEST_SIZE)) {
+            if (memcmp(existing_hash, cfg.empty_hash, SHA512_DIGEST_SIZE)) {
                 // hashes do not match
                 // yes, output_file->_fileno is crazy and non-portable...
                 fallocate(output_file->_fileno, FALLOC_FL_PUNCH_HOLE || FALLOC_FL_KEEP_SIZE,
-                          (__off_t) (current_block * blocksize), (__off_t) blocksize);
+                          (__off_t) (current_block * cfg.blocksize), (__off_t) cfg.blocksize);
                 std::fflush(output_file);
                 // write hash
                 std::fseek(hash_file, current_block * SHA512_DIGEST_SIZE, SEEK_SET);
-                std::fwrite(empty_hash_uint64_t, 1, SHA512_DIGEST_SIZE, hash_file);
+                std::fwrite(cfg.empty_hash, 1, SHA512_DIGEST_SIZE, hash_file);
                 std::fflush(hash_file);
             }
         } else {
@@ -113,7 +117,7 @@ void hash_thread(
             if (memcmp(existing_hash, new_hash, SHA512_DIGEST_SIZE) != 0) {
                 // hashes do not match
                 // output file
-                std::fseek(output_file, current_block * blocksize, SEEK_SET);
+                std::fseek(output_file, current_block * cfg.blocksize, SEEK_SET);
                 std::fwrite(block_buffer, 1, rw_size, output_file);
                 std::fflush(output_file);
                 // hash file
@@ -138,22 +142,70 @@ void hash_thread(
 
 
 int main(int argc, char const *argv[]) {
-    if (argc < 3) {
-        std::cout << "Requires at least 3 arguments" << std::endl;
+
+    if (argc < 1) {
+        std::cerr << "Must supply config file!" << std::endl;
         return 1;
     }
 
-    if (std::stoi(argv[3]) < 0) {
-        std::cerr << "Cannot have negative blocksize!" << std::endl;
+    config_struct cfg;
+    boost::property_tree::ptree test_tree;
+    try {
+        boost::property_tree::json_parser::read_json(argv[1], test_tree);
+        cfg.current_block = 0;
+        cfg.input_file_path = test_tree.get<std::string>("input");
+        cfg.output_file_path = test_tree.get<std::string>("output");
+        cfg.hash_file_path = cfg.output_file_path + ".hash";
+        cfg.blocksize = test_tree.get<ullong>("blocksize");
+        try {
+            cfg.thread_cout = test_tree.get<int>("threads");
+        } catch (cfg_field_not_found) {
+            cfg.thread_cout = 1;
+        }
+        try {
+            cfg.output_interval = test_tree.get<int>("output interval");
+        } catch (cfg_field_not_found) {
+            cfg.output_interval = 1;
+        }
+        try {
+            cfg.use_sparse_output = test_tree.get<bool>("sparse output");
+        } catch (cfg_field_not_found) {
+            cfg.use_sparse_output = false;
+        }
+        try {
+            cfg.do_status_update = test_tree.get<bool>("status update");
+        } catch (cfg_field_not_found) {
+            cfg.do_status_update = true;
+        }
+    } catch (cfg_field_not_found &e) {
+        std::cerr << "Could not parse config file!" << std::endl;
+        std::cerr << "Reason: ";
+        std::cerr << e.what() << std::endl;
+        return 1;
+    } catch (...) {
+        std::cerr << "Could not parse config file!" << std::endl;
+        return 2;
+    }
+    if (cfg.blocksize < 1) {
+        std::cerr << "Config error: blocksize too small!" << std::endl;
         return 1;
     }
-    ullong blocksize = (ullong) std::stoi(argv[3]);
+    if (cfg.thread_cout < 1) {
+        std::cerr << "Config error: need at least one thread!" << std::endl;
+        return 1;
+    }
+    if (cfg.output_interval < 0) {
+        std::cerr << "Config error: cannot output at negetive interval!" << std::endl;
+        return 1;
+    }
 
-    std::ifstream input_stream(argv[1], std::ifstream::binary | std::ifstream::ate);
+    // make sure we can open the necessary files
+    std::ifstream input_stream(cfg.input_file_path, std::ifstream::binary | std::ifstream::ate);
     if (!input_stream) {
         std::cout << "Could not open input file." << std::endl;
         return 1;
     }
+
 
     // in lined from block device size
     std::string arg_1_str(argv[1]);
@@ -165,24 +217,22 @@ int main(int argc, char const *argv[]) {
             (ullong) input_stream.tellg()
     );
     input_stream.seekg(0);
-    // divide and round up
-    ullong n_blocks((input_size + blocksize - 1) / blocksize);
 
-    std::string hash_filename(argv[2]);
-    hash_filename += ".hash";
-    std::fstream hash_stream(hash_filename, std::fstream::binary | std::fstream::in | std::fstream::out);
+    // divide and round up
+    cfg.n_blocks = ((input_size + cfg.blocksize - 1) / cfg.blocksize);
+
+    // make sure we can open the necessary files
+    std::fstream hash_stream(cfg.hash_file_path, std::fstream::binary | std::fstream::in | std::fstream::out);
     if (!hash_stream) {
         std::cout << "Could not open hash file. Make one with:" << std::endl;
-        std::cout << "truncate -s " << n_blocks * SHA512_DIGEST_SIZE << " " << hash_filename <<
-        std::endl;
-        return 1;
+        std::cout << "truncate -s " << cfg.n_blocks * SHA512_DIGEST_SIZE << " " << cfg.hash_file_path << std::endl;
+        return 3;
     }
-
-    std::fstream output_stream(argv[2], std::fstream::binary | std::fstream::in | std::fstream::out);
+    std::fstream output_stream(cfg.output_file_path, std::fstream::binary | std::fstream::in | std::fstream::out);
     if (!output_stream) {
         std::cout << "Could not open output file. Make one with:" << std::endl;
-        std::cout << "truncate -s " << input_size << " " << argv[2] << std::endl;
-        return 1;
+        std::cout << "truncate -s " << input_size << " " << cfg.output_file_path << std::endl;
+        return 4;
     }
 
     // the threads have to open their own files
@@ -190,32 +240,27 @@ int main(int argc, char const *argv[]) {
     output_stream.close();
     hash_stream.close();
 
-    std::atomic_ullong current_block(0);
-    std::string input_filename(argv[1]);
-    std::string output_filename(argv[2]);
-
-    char *empty_block = new char[blocksize];
-    uint64_t *empty_hash_uint64_t = new uint64_t[SHA512_DIGEST_SIZE / sizeof(uint64_t)];
-    memset(empty_block, 0, blocksize);
-    sha512_buffer(empty_block, blocksize, empty_hash_uint64_t);
+    cfg.empty_block = new char[cfg.blocksize];
+    cfg.empty_hash = new char[SHA512_DIGEST_SIZE];
+    memset(cfg.empty_block, 0, cfg.blocksize);
+    sha512_buffer(cfg.empty_block, cfg.blocksize, cfg.empty_hash);
 
     std::list<std::thread> threads;
-    for (int n = 0; n < THREAD_COUNT; n++) {
+    for (int n = 0; n < cfg.thread_cout; n++) {
         // constructs them in-place, so we don't have to worry about a temp var or copy-insertion or anything
-        threads.emplace_back(&hash_thread,
-                             std::ref(current_block),
-                             std::cref(input_filename),
-                             std::cref(output_filename),
-                             n_blocks,
-                             blocksize,
-                             empty_block,
-                             empty_hash_uint64_t
-        );
+        threads.emplace_back(&hash_thread, std::ref(cfg));
     }
-    auto display_thread = std::thread(progress_thread, std::ref(current_block), n_blocks);
+    std::thread display_thread;
+    if (cfg.do_status_update) {
+        display_thread = std::thread(progress_thread, std::ref(cfg));
+    }
+
     for (auto &t : threads) {
         t.join();
     }
-    display_thread.join();
+
+    if (cfg.do_status_update) {
+        display_thread.join();
+    }
     return 0;
 }
